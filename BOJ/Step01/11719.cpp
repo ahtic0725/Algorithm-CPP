727c87ab27a2ba03fd959638d119a05a6247d00a
ahtic0725 <ahtic0725@gmail.com>
2018-07-25 11:09:36 +0900

    BOJ Step01 11719

---
+++ BOJ/Step01/11719.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    char c;
+    while (1) {
+        c = getchar();
+        if (c == EOF)
+            break;
+        putchar(c);
+    }
+}
