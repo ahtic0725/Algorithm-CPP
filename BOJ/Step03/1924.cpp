cef8eb051e837feaddc2bc485a147bf334e47fe1
ahtic0725 <ahtic0725@gmail.com>
2018-07-25 11:43:32 +0900

    BOJ Step03 1924

---
+++ BOJ/Step03/1924.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int day[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    char mon[7][4] = {
+        "SUN",
+        "MON",
+        "TUE",
+        "WED",
+        "THU",
+        "FRI",
+        "SAT"
+    };
+    int x, y, res = 0;
+    scanf("%d %d", &x, &y);
+    for (int i = 0; i < x - 1; ++i)
+        res += day[i];
+    
+    printf("%s", mon[(res + y) % 7]);
+}
